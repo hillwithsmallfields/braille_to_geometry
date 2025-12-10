7fd8d623e74483a1af030e8b32a2c49fe8df1d7f
John Sturdy <jcg.sturdy@gmail.com>
2025-12-10 13:49:07 +0000

    Add project files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="braille_to_geometry",
+    version="0.1",
+    description="Convert text strings, as braille, to shapely geometries.",
+    author="John C. G. Sturdy",
+    author_email="jcg.sturdy@gmail.com",
+    packages=find_packages(),
+)
